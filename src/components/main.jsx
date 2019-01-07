8b48cc66c2d63b98d0b7145247902cbe5d41b8d8
Lucas Cheung <lucascheungls@gmail.com>
2019-01-08 00:12:17 +0100

    initial commit

---
+++ src/components/main.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+class Main extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>OG Marketing</h1>
+        <p>Let the experts do it for you.</p>
+      </div>
+    );
+  }
+}
+
+export default Main;
